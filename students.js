452e2f16a1238df0f77973bb7c1d4b95913ab26a
damian <photoca@hotmail.com>
2022-08-01 16:07:13 -0400

    first web server

--- students.js
+++ students.js
@@ -10,11 +10,16 @@ let students = ["bob","samantha","lily","grace","jim"]
 
 app.get("/",(req,res)=> {
 
-    let output = "<html><body>" // starts
+    let output = "<html><body><ul>" // starts
     
-    output = output + 'hello there html' //middle
+    //output = output + 'hello there html' //middle
+    for(let i = 0; i < students.length;i++) {
+        output = output + 
+        '<li><input type="checkbox">' + 
+        students[i] + '</li>'
+    }
 
-    output = output + "</body></html>" //end
+    output = output + "</ul></body></html>" //end
 
     res.send(output)
 })
