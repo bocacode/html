6dbf03360322b7918e9418e4701440a8778f78f3
damian <photoca@hotmail.com>
2022-08-01 14:28:10 -0400

    latest

--- students.js
+++ students.js
@@ -6,10 +6,19 @@ const port = 4001
 app.use(cors())
 app.use(express.json())
 
+let students = ["bob","samantha","lily","grace","jim"]
+
 app.get("/",(req,res)=> {
-    res.send("hello world")
+
+    let output = "<html><body><h1>" // starts
+    
+    output = output + "hello there html" //middle
+
+    output = output + "</h1></body></html>" //end
+
+    res.send(output)
 })
 
 app.listen(port,()=> {
-    console.log("I was needed")
+    console.log("localhost:4000")
 })
